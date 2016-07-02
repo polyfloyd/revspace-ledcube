31f93be0de261734781d46207b4258ceb1781df3
polyfloyd <floyd@polyfloyd.net>
2016-07-03 00:22:55 +0200

    Add some documentation

--- frame.h
+++ frame.h
@@ -1,7 +1,15 @@
 #include "stdint.h"
 
+// Calculates the absolute index in a frame from an XYZ-coordinate.
 #define FR_IDX(x, y, z) (z * 64 + y * 8 + x)
 
+// Applies the specified function to each LED in the frame. The function should
+// return the respective grayscale value.
 void map3D(uint8_t *frame, uint8_t (*func)(uint8_t, uint8_t, uint8_t));
+
+// Applies the specified function to each column. The functon should return the
+// coordinate of the LED along the Z-axis to set to full power.
 void map2D(uint8_t *frame, uint8_t (*func)(uint8_t, uint8_t));
+
+// Sets all LEDs in the frame to the same value.
 void setAll(uint8_t *frame, uint8_t value);
--- revcube.ino
+++ revcube.ino
@@ -12,6 +12,8 @@
 volatile void refreshCallback();
 void draw(void (*func)(uint8_t*));
 
+// This list contains the pin mapping for the Z-axis. Ports are rawly
+// manipulated for more speed!
 struct {
     uint8_t bit;
     volatile uint8_t *ctl;
@@ -27,11 +29,14 @@ struct {
     { 1<<1,  &DDRC, &PORTC }, // Pin A1
 };
 
+// This table maps the frame's grayscale values (0 <= v < 4096) to the value
+// that the TLCs (0 <= v < 4096). An inversion is applied to match the pullup
+// resistors and a logarithm is used to make the frame grayscale more linear in
+// light output.
 static uint16_t grayscaleTable[255];
 
 volatile static uint8_t currentBufferIndex = 0;
 volatile static uint8_t buffer[512 * NUM_BUFFERS] = { 0 };
-volatile static uint8_t *shownFrame = &buffer[currentBufferIndex * 512];
 volatile static uint8_t *newFrame = 0;
 
 void setup() {
@@ -52,27 +57,41 @@ void loop() {
     });
 }
 
+// This function can be used to draw using a double buffer and automaticaly
+// swap to it. The specified function should asume that not all values in the
+// argument buffer are set to 0.
 void draw(void (*func)(uint8_t*)) {
+    // Cycle to the next buffer and get a pointer to the beginning of it.
     currentBufferIndex = (currentBufferIndex + 1) % NUM_BUFFERS;
     volatile uint8_t *buf = &buffer[currentBufferIndex * 512];
+    // Apply the draw function.
     func((uint8_t*)buf);
+    // Make the frame available to the refresh callback.
     newFrame = buf;
 }
 
+// This function is run every time a TLC grayscale cycle has been completed.
 volatile void refreshCallback() {
     volatile static uint8_t shownZ = 0;
+    volatile static uint8_t *shownFrame = &buffer[currentBufferIndex * 512];
 
+    // Turn off the previous level.
     PORT_LO(*zPins[shownZ].port, zPins[shownZ].bit);
+    // Cycle to the next level.
     shownZ = (shownZ + 1) % 8;
+    // If a frame has been completed and a new frame is available, set it as
+    // the shown frame.
     if (!shownZ && newFrame) {
         shownFrame = newFrame;
         newFrame = 0;
     }
+    // Write the current level data to the TLCs.
     for (int x = 0; x < 8; x++) {
         for (int y = 0; y < 8; y++) {
             Tlc.set(y * 8 + x, grayscaleTable[shownFrame[FR_IDX(x, y, shownZ)]]);
         }
     }
     Tlc.update();
+    // Turn on the level to display the current level.
     PORT_HI(*zPins[shownZ].port, zPins[shownZ].bit);
 }
